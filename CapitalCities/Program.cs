0dcfdeb31331752e494d9a180aa15a0b9ff2dc21
Martin Červený <martin.cerveny@sielaff.cz>
2024-05-13 09:10:16 +0200

    Revert "separate games as unique instances"
    This reverts commit 86644544acfbbde305f14bcf365d7accb8244df5.

--- CapitalCities/Program.cs
+++ CapitalCities/Program.cs
@@ -4,17 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        bool playAgain = true;
-
-        while (playAgain)
-        {
-            Game game = new Game();
-            game.Start();
-
-            Console.WriteLine("Do you want to play again? (yes/no): ");
-            string playAgainInput = Console.ReadLine();
-
-            playAgain = playAgainInput.Equals("yes", StringComparison.InvariantCultureIgnoreCase);
-        }
+        Game game = new Game();
+        game.Start();
     }
 }
